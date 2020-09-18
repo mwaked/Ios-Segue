60851cea6e49e82449c168aecf55892da912d0f1
Mahmoud Waked <mwaked@gmail.com>
2020-09-19 00:24:08 +0200

    Initial Commit

---
+++ PassingData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PassingData
+//
+//  Created by Mahmoud Waked on 9/19/20.
+//  Copyright © 2020 BIM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
